854f60ed56c22d88357a5323f7d586569a31e4d7
Neeraj Yadav <35502764+neerajyadv@users.noreply.github.com>
2018-04-15 17:26:51 +0530

    Create FirstPage.dart

---
+++ FirstPage.dart
@@ -0,0 +1,10 @@
+class First extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: new Center(
+        child: new Icon(Icons.accessibility, size: 150.0, color: Colors.orange),
+      ),
+    );
+  }
+}
